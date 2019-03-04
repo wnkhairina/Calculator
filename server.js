d4fc56556a72986fdcc96d29f0632bfa086a6b48
wnkhairina <wnkhairina@gmail.com>
2019-03-05 00:15:05 +0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,23 @@
+
+const Hapi = require('hapi');
+
+const host = 'localhost';
+const port = 3000; 
+
+
+const server = Hapi.Server({
+    host: host,
+    port: port
+});
+
+const init = async () => {
+
+    await server.start();
+    console.log("Server up and running at port: " + port);
+
+}
+
+require('./routes/routes')(server);
+
+
+init();
